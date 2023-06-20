ac8bba2b26778ba28143568fca0cb9ebaa43a158
siddharth821 <siddhartha.singh821@gmail.com>
2023-06-20 19:22:07 +0530

    Intro added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import Carousels from '../components/Carousels'
-import './Home.css';
+import Intro from "./Intro";
+import Carousels from "../components/Carousels";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <>
-            <div className="home">
-                <h3>intro section</h3>
-            </div>
-
-            <Carousels/>
-        </>
-    );
+  return (
+    <>
+      <Intro />
+      <Carousels />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/pages/Intro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Intro.css";
+import DisabledContext from "antd/es/config-provider/DisabledContext";
+const Intro = () => {
+  return (
+    <div className="container">
+      <h1>NullStrange Audio Production</h1>
+      <p>
+        NullStrange Audio Production is a professional recording and audio
+        production company specializing in top-quality music production, sound
+        engineering, mixing, and mastering services. We use the latest
+        technology to create the best sounding recordings and offer fast
+        turnaround times to ensure your projects are completed quickly and to
+        the highest standard. Our team of experienced sound engineers and
+        producers will help you achieve the sound you’re looking for and deliver
+        the best results.
+      </p>
+    </div>
+  );
+};
+export default Intro;
