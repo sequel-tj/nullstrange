646b798e1c95ea843471732be4fec704c8773187
siddharth821 <siddhartha.singh821@gmail.com>
2023-06-22 16:57:13 +0530

    footer updated

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -8,27 +8,53 @@ const Footer = () => {
         <div class="footer__redes">
           <ul class="footer__redes-wrapper">
             <li>
-              <a href="#" class="footer__link">
-                <img src={require("../images/facebook.png")} />
-                facebook
+              <a
+                href="https://twitter.com/saranshSCS"
+                target="_blank"
+                class="footer__link"
+              >
+                <img src={require("../images/twitter-sign.png")} />
+                twitter
               </a>
             </li>
             <li>
-              <a href="#" class="footer__link">
+              <a
+                href="https://open.spotify.com/artist/6VNdlOegPizz5bD6mN54EO?si=HEuS52WUQNu1Hkmu6CC1mA"
+                target="_blank"
+                class="footer__link"
+              >
                 <img src={require("../images/social.png")} />
                 spotify
               </a>
             </li>
             <li>
-              <a href="#" class="footer__link">
+              <a
+                href="https://www.instagram.com/nullstrange/"
+                target="_blank"
+                class="footer__link"
+              >
                 <img src={require("../images/instagram.png")} />
                 instagram
               </a>
             </li>
             <li>
-              <a href="#" class="footer__link">
+              <a
+                href="https://youtube.com/@NullStrange"
+                target="_blank"
+                class="footer__link"
+              >
                 <img src={require("../images/youtube.png")} />
                 youtube
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:nullstrange01@gmail.com"
+                target="_blank"
+                class="footer__link"
+              >
+                <img src={require("../images/email.png")} />
+                e-mail
               </a>
             </li>
           </ul>
