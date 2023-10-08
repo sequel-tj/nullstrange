ad004cde1438efc197609a287151f350643b8c57
siddharth821 <siddhartha.singh821@gmail.com>
2023-10-08 17:20:35 +0530

    beats page changes

--- src/components/Beat.jsx
+++ src/components/Beat.jsx
@@ -1,9 +1,7 @@
 import React, { useState ,useRef, useEffect} from "react";
-import audioSrc from "../audio/bewafa.mp3";
 const Beat = (props) => {
+  const audioSrc  = props.src;
   const [cartadd, setcartadd] = useState("Add To Cart");
-  const [isHovering1, setIsHovering1] = useState(false);
-  const [isHovering, setIsHovering] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0); 
@@ -38,16 +36,13 @@ const Beat = (props) => {
     }
     useEffect(() => {
         audioRef.current.addEventListener("timeupdate", handleTimeUpdate);
-        return () => {
-          audioRef.current.removeEventListener("timeupdate", handleTimeUpdate);
-        };
-      }, []);
+      }, [audioRef]);
   const container: React.CSSProperties = {
     margin: "10vh",
     color: "white",
     padding: "10px 10px 0 10px",
-    width: "200px",
-    height: "300px",
+    width: "300px",
+    height: "400px",
     background: "rgb(20 20 20 / 70%)",
     border: "1px solid white",
     overflow: "hidden",
@@ -57,19 +52,20 @@ const Beat = (props) => {
     alignItems: "center",
     borderRadius: "12px",
   };
-  const audio: React.CSSProperties = {
-    width: "70px",
+  const album: React.CSSProperties = {
+    height: "240px",
+    width: "240px",
+    borderRadius: "12px",
+    cursor: "pointer",
   };
   const image: React.CSSProperties = {
-    marginTop: "20px",
-    height: "80px",
-    width: "80px",
-    cursor: `${isHovering ? "pointer" : "default"}`,
+    height: "45px",
+    width: "45px",
+    cursor: "pointer",
   };
-
   const songName: React.CSSProperties = {
-    margin: "5px",
-    fontSize: "25px",
+    margin: "2px",
+    fontSize: "20px",
     fontFamily: "Lucida Handwriting, cursive",
     textAlign: "front",
     letterSpacing: ".05em",
@@ -77,33 +73,45 @@ const Beat = (props) => {
   const cart: React.CSSProperties = {
     marginTop: "auto",
     color: "white",
-    width: "200px",
+    width: "300px",
     height: "50px",
     border: "1px solid white",
     overflow: "hidden",
     display: "flex",
     justifyContent: "center",
     borderRadius: "0  0 12px 12px",
-    cursor: `${isHovering1 ? "pointer" : "default"}`,
+    cursor:"pointer",
   };
+  const player: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "5px",
+    gap: "18px",
+  }
+  const playerContainer: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "column",
+    width: "200px",
+  }
   const inp: React.CSSProperties = {
     width: '100%',
   height: '3px',
+  marginTop: '14px',
   accentColor: "rgb(117 69 112)",
   outline: "none",
-  marginTop: "50px",
   };
   const trackDuration: React.CSSProperties = {
     display: "flex",
   alignItems: "center",
   justifyContent:"space-between",
-  marginTop:"10px",
+  margin:"4px",
+  // gap: "90px",
   };
   const trackDurationP: React.CSSProperties = {
     fontSize: "10px",
   fontWeight: "500",
   color: "#181,1F",
-  margin: "0 60px 0 60px",
   };
   const cartText: React.CSSProperties = {
     padding: "8px",
@@ -111,18 +119,6 @@ const Beat = (props) => {
     fontFamily: "Lucida Handwriting, cursive",
     textAlign: "front",
     letterSpacing: ".05em",
-  };
-  const hoverin1 = () => {
-    setIsHovering1(true);
-  };
-  const hoverout1 = () => {
-    setIsHovering1(false);
-  };
-  const hoverin = () => {
-    setIsHovering(true);
-  };
-  const hoverout = () => {
-    setIsHovering(false);
   };
   const onClickHandler1 = () => {
     if (cartadd === "Add To Cart") setcartadd("Remove");
@@ -132,35 +128,35 @@ const Beat = (props) => {
     <div style={container}>
       <h2 style={songName}>{props.name}</h2>
       
-    
+          <img src = {props.img} style = {album}></img>
+      <div style = {player}>
           <img
-        src={require("../images/play.png")}
-        alt="beat-img"
-        style={image}
-        onClick={handlePlayPause}
-        onMouseEnter={hoverin}
-        onMouseLeave={hoverout}
-      />
-      <input
-            style={inp}
-            type="range"
-            min="0"
-            max={duration}
-            value={currentTime}
-            onChange={handleSeek}
+            src={ isPlaying ? require("../images/pause.png") : require("../images/play.png")}
+            alt="beat-img"
+            style={image}
+            onClick={handlePlayPause}
           />
-    
-          <audio ref={audioRef} src = {audioSrc}></audio>
-    
-          <div style = {trackDuration}>
-            <p style = {trackDurationP}>{formatDuration(currentTime)}</p>
-            <p style = {trackDurationP}>{formatDuration(duration)}</p>
+          <div style = {playerContainer}>
+                <input
+                  style={inp}
+                  type="range"
+                  min="0"
+                  max={duration}
+                  value={currentTime}
+                  onChange={handleSeek}
+                />
+          
+                <audio ref={audioRef} src = {audioSrc}></audio>
+          
+                <div style = {trackDuration}>
+                  <p style = {trackDurationP}>{formatDuration(currentTime)}</p>
+                  <p style = {trackDurationP}>{formatDuration(duration)}</p>
+                </div>
           </div>
+      </div>
       <div
         style={cart}
         onClick={onClickHandler1}
-        onMouseEnter={hoverin1}
-        onMouseLeave={hoverout1}
       >
         <h2 style={cartText}>{cartadd}</h2>
       </div>
--- src/pages/Beats.jsx
+++ src/pages/Beats.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Beat from "../components/Beat";
+import frost from "../audio/Frost.mp3";
+import gunman from "../audio/Gunman.mp3";
+import rahasya from "../audio/Rahasya.mp3";
+import frostimg from "../images/Frost.png";
+import gunmanimg from "../images/Gunman.png";
+import rahasyaimg from "../images/Rahasya.png";
 const Beats = () => {
   const container: React.CSSProperties = {
     display: "flex",
@@ -8,26 +14,22 @@ const Beats = () => {
     height: "100vh",
     justifyContent: "center",
   };
-  const name = [
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
-    "bewafa",
+  const nam = [
+    "Frost","Gunman","Rahasya"
   ];
+  const audioSrc = [
+    frost,gunman,rahasya
+  ];
+  const albumCover = [
+    frostimg, gunmanimg, rahasyaimg
+  ];
+  const ct = [0,1,2];
   return (
     <div>
       <h3>BEATS</h3>
       <div style={container}>
-        {name.map((value) => (
-          <Beat name={value}></Beat>
+        {ct.map((i) => (
+          <Beat name = {nam[i]} src = {audioSrc[i]} img = {albumCover[i]}></Beat>
         ))}
       </div>
     </div>
